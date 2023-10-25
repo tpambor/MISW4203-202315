7a94e248c444d9c43739ac8b53a205b102e7cbf0
Tim Pambor <t.pambor@uniandes.edu.co>
2023-10-25 22:15:54 +0200

    Implementar navegación (#91)

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/MainActivity.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/MainActivity.kt
@@ -4,43 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import co.edu.uniandes.misw4203.equipo11.vinilos.ui.theme.VinilosTheme
+import co.edu.uniandes.misw4203.equipo11.vinilos.views.NavBar
+import co.edu.uniandes.misw4203.equipo11.vinilos.views.NavContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            val currentBackStackEntry by navController.currentBackStackEntryAsState()
+
             VinilosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Scaffold(
+                    content = { padding ->
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(padding),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            NavContent(navController)
+                        }
+                    },
+                    bottomBar = {
+                        NavBar(navController, currentBackStackEntry)
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VinilosTheme {
-        Greeting("Android")
     }
 }
