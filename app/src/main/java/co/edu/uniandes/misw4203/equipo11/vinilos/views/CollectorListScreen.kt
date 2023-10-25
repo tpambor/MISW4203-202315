4a71a4b51e0223cdd564451ec17c27394a0662c0
Tim Pambor <t.pambor@uniandes.edu.co>
2023-10-25 11:45:50 +0200

    Implementar navegación

---
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/views/CollectorListScreen.kt
@@ -0,0 +1,9 @@
+package co.edu.uniandes.misw4203.equipo11.vinilos.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CollectorListScreen() {
+    Text("Listado de coleccionistas")
+}
