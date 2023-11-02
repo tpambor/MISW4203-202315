1146f39c4da13209c7512d1382b86df20c279c45
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2023-11-01 22:45:14 -0500

    Planteamiento inicial e2e

---
+++ app/src/androidTest/java/co/edu/uniandes/misw4203/equipo11/vinilos/pageobjects/ArtistList.kt
@@ -0,0 +1,14 @@
+package co.edu.uniandes.misw4203.equipo11.vinilos.pageobjects
+
+import androidx.compose.ui.test.SemanticsNodeInteractionCollection
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import co.edu.uniandes.misw4203.equipo11.vinilos.MainActivity
+class ArtistList(composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) : PageObject(composeTestRule) {
+    fun getArtists(): SemanticsNodeInteractionCollection {
+        return findAtLeastOne(
+            hasTestTag("artist-list-item")
+        )
+    }
+}
