25762b6e3258cc6ad6fd4464231482dcff067dde
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2023-11-01 19:51:58 -0500

    Botón favorito - versión inicial

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/views/ArtistListScreen.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/views/ArtistListScreen.kt
@@ -12,9 +12,14 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
@@ -25,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -97,6 +103,10 @@ fun ArtistListScreen() {
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
 @Composable
 private fun ArtistItem(performer: Performer) {
+    var isFavorite by remember { mutableStateOf(false) }
+
+    // TODO: Agregar lógica para manejar la acción de agregar/quitar de favoritos
+
     var coverPreview: Placeholder? = null
     if (LocalInspectionMode.current) {
         coverPreview = placeholder(ColorPainter(Color(performer.image.toColorInt())))
@@ -107,7 +117,7 @@ private fun ArtistItem(performer: Performer) {
             .testTag("performer-list-item"),
         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background),
         shape = RectangleShape,
-        onClick = { /*TODO*/ }
+        onClick = { /*TODO: Acción para llevar al detalle*/ }
     ) {
         Column {
             GlideImage(
@@ -120,6 +130,21 @@ private fun ArtistItem(performer: Performer) {
                     .aspectRatio(1f),
                 contentScale = ContentScale.Crop
             )
+            // Favorite button
+            IconButton(
+                onClick = {
+                isFavorite = !isFavorite
+                // TODO: Agregar lógica para manejar la acción de agregar/quitar de favoritos
+                },
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(4.dp)
+            ) {
+                Icon(
+                    imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                    contentDescription = "Agregar a favoritos"
+                )
+            }
             Text(
                 text = performer.name,
                 modifier = Modifier
