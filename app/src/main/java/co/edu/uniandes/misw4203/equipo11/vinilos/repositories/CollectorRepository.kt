ad3158273ceec7eda399962a3b1c6f7a3662bffe
Tim Pambor <t.pambor@uniandes.edu.co>
2023-11-05 00:23:17 +0100

    Implementar CollectorListScreen y CollectorListViewModel

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/repositories/CollectorRepository.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/repositories/CollectorRepository.kt
@@ -1,16 +1,19 @@
 package co.edu.uniandes.misw4203.equipo11.vinilos.repositories
 
 import android.util.Log
-import co.edu.uniandes.misw4203.equipo11.vinilos.models.CollectorDAO
 import co.edu.uniandes.misw4203.equipo11.vinilos.models.Collector
+import co.edu.uniandes.misw4203.equipo11.vinilos.models.CollectorWithPerformers
+import co.edu.uniandes.misw4203.equipo11.vinilos.models.Performer
 import co.edu.uniandes.misw4203.equipo11.vinilos.models.VinilosDB
 import co.edu.uniandes.misw4203.equipo11.vinilos.network.NetworkServiceAdapter
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 interface ICollectorRepository {
     fun getCollectors(): Flow<List<Collector>?>
+    fun getCollectorsWithPerformers(): Flow<List<CollectorWithPerformers>?>
     suspend fun refresh(): Boolean
 }
 
@@ -30,13 +33,29 @@ class CollectorRepository : ICollectorRepository {
         }
     }
 
+    override fun getCollectorsWithPerformers(): Flow<List<CollectorWithPerformers>?> {
+        // TODO: Get performers from database
+        val performers: List<Performer> = listOf(
+            Performer(1, "Rubén Blades Bellido de Luna", "Red", "description"),
+            Performer(2, "Queen", "Red", "description"),
+            Performer(3, "The Beatles", "Red", "description"),
+            Performer(4, "Calle 13", "Red", "description"),
+        )
+
+        return getCollectors().map { collectors ->
+            collectors?.map { collector ->
+                CollectorWithPerformers(collector, performers)
+            }
+        }
+    }
+
     override suspend fun refresh(): Boolean {
         val collectors: List<Collector>?
 
         try {
             collectors = adapter.getCollectors().first()
         } catch (ex: Exception) {
-            Log.e(CollectorRepository.TAG, "Error loading Collectors: $ex")
+            Log.e(TAG, "Error loading Collectors: $ex")
             return false
         }
 
---
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/viewmodels/CollectorListViewModel.kt
@@ -0,0 +1,65 @@
+package co.edu.uniandes.misw4203.equipo11.vinilos.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import co.edu.uniandes.misw4203.equipo11.vinilos.R
+import co.edu.uniandes.misw4203.equipo11.vinilos.models.CollectorWithPerformers
+import co.edu.uniandes.misw4203.equipo11.vinilos.repositories.ICollectorRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class CollectorListViewModel(val collectorRepository: ICollectorRepository) : ViewModel() {
+    private val _collectors: MutableStateFlow<List<CollectorWithPerformers>> = MutableStateFlow(emptyList())
+    val collectors = _collectors.asStateFlow()
+
+    private val _isRefreshing = MutableStateFlow(true)
+    val isRefreshing = _isRefreshing.asStateFlow()
+
+    private val _error = MutableStateFlow<ErrorUiState>(ErrorUiState.NoError)
+    val error = _error.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            collectorRepository.getCollectorsWithPerformers()
+                .collect { collectors ->
+                    if (collectors == null) {
+                        _error.value = ErrorUiState.Error(R.string.network_error)
+                    } else {
+                        _collectors.value = collectors
+                        _error.value = ErrorUiState.NoError
+                    }
+                    _isRefreshing.value = false
+                }
+        }
+    }
+
+    fun onRefresh() {
+        _isRefreshing.value = true
+        _error.value = ErrorUiState.NoError
+
+        viewModelScope.launch {
+            if (!collectorRepository.refresh()) {
+                _isRefreshing.value = false
+                _error.value = ErrorUiState.Error(R.string.network_error)
+            }
+        }
+    }
+
+    // ViewModel factory
+    companion object {
+        val KEY_COLLECTOR_REPOSITORY = object : CreationExtras.Key<ICollectorRepository> {}
+
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                CollectorListViewModel(
+                    collectorRepository = requireNotNull(this[KEY_COLLECTOR_REPOSITORY]),
+                )
+            }
+        }
+    }
+}
