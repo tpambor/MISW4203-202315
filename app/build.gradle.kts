e617e897b81336cac95c7762c1c81adb87647b2f
Tim Pambor <t.pambor@uniandes.edu.co>
2023-11-08 14:42:01 +0100

    Bump version para iteración 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "co.edu.uniandes.misw4203.equipo11.vinilos"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
