c534ea22cd0f96bd763176e1343ad3142195cfb8
Laura Molina <ld.molina11@uniandes.edu.co>
2023-11-08 21:36:45 -0500

    Previews

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/ui/views/ArtistDetailScreen.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/ui/views/ArtistDetailScreen.kt
@@ -1,10 +1,38 @@
 package co.edu.uniandes.misw4203.equipo11.vinilos.ui.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.Performer
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.PerformerType
+import co.edu.uniandes.misw4203.equipo11.vinilos.ui.theme.VinilosTheme
+import java.time.Instant
 
 @Composable
 fun ArtistDetailScreen(snackbarHostState: SnackbarHostState, artistId: Int?){
     Text("Detalle artista id: ${artistId}")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ArtistDetailScreenPreview() {
+    var performer = Performer(1, PerformerType.MUSICIAN,"Rubén Blades Bellido de Luna","red", "Es un cantante, compositor, músico, actor, abogado, político y activista panameño. Ha desarrollado gran parte de su carrera artística en la ciudad de Nueva York.", Instant.now())
+
+    VinilosTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column {
+                
+            }
+        }
+    }
+
 }
