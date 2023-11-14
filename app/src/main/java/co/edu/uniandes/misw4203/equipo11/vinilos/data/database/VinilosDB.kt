c0960574838d0c29c7d293bd509f36b967ac4399
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2023-11-14 16:33:26 -0500

    Relación de clases con Album - DB

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/data/database/VinilosDB.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/data/database/VinilosDB.kt
@@ -9,16 +9,26 @@ import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.daos.PerformerDAO
 import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.Album
 import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.Collector
 import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.CollectorFavoritePerformer
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.Comment
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.CommentAlbum
 import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.Performer
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.PerformerAlbum
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.Track
+import co.edu.uniandes.misw4203.equipo11.vinilos.data.database.models.TrackAlbum
 
 @Database(
     entities = [
         Album::class,
         Collector::class,
         CollectorFavoritePerformer::class,
-        Performer::class
+        Performer::class,
+        PerformerAlbum ::class,
+        CommentAlbum::class,
+        TrackAlbum::class,
+        Track::class,
+        Comment::class
     ],
-    version = 3,
+    version = 6,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
