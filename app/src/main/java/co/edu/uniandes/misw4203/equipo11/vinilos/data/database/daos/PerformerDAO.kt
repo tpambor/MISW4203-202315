b04e3386268577842467fc4e10f6148aeea8c05c
Tim Pambor <t.pambor@uniandes.edu.co>
2023-11-16 23:37:06 +0100

    Cambiar DAO a abstract class para esconder metodos internos

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/data/database/daos/PerformerDAO.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/data/database/daos/PerformerDAO.kt
@@ -20,77 +20,75 @@ import co.edu.uniandes.misw4203.equipo11.vinilos.data.network.models.MusicianJso
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface PerformerDAO {
+abstract class PerformerDAO {
     @Query("SELECT * FROM performer WHERE type = :performerType ORDER BY name COLLATE UNICODE")
-    fun getPerformersByType(performerType: PerformerType): Flow<List<Performer>>
+    protected abstract fun getPerformersByType(performerType: PerformerType): Flow<List<Performer>>
 
     @Query("SELECT * FROM performer WHERE id = :performerId AND type = :performerType")
-    fun getPerformersByTypeId(performerType: PerformerType, performerId: Int): Flow<Performer?>
+    protected abstract fun getPerformerByTypeId(performerType: PerformerType, performerId: Int): Flow<Performer?>
 
     fun getMusicians(): Flow<List<Performer>> = getPerformersByType(PerformerType.MUSICIAN)
 
-    fun getMusicianById(performerId: Int): Flow<Performer?> = getPerformersByTypeId(PerformerType.MUSICIAN, performerId)
+    fun getMusicianById(performerId: Int): Flow<Performer?> = getPerformerByTypeId(PerformerType.MUSICIAN, performerId)
 
-    fun getBandById(performerId: Int): Flow<Performer?> = getPerformersByTypeId(PerformerType.BAND, performerId)
+    fun getBandById(performerId: Int): Flow<Performer?> = getPerformerByTypeId(PerformerType.BAND, performerId)
 
     fun getBands(): Flow<List<Performer>> = getPerformersByType(PerformerType.BAND)
 
-    @Query("SELECT p.* FROM MusicianBand mb JOIN Performer p ON mb.musicianId = p.id WHERE bandId = :performerId")
-    fun getBandMembers(performerId: Int): Flow<List<Performer>>
+    @Query("SELECT p.* FROM MusicianBand mb JOIN Performer p ON mb.musicianId = p.id WHERE bandId = :performerId ORDER BY p.name COLLATE UNICODE")
+    abstract fun getBandMembers(performerId: Int): Flow<List<Performer>>
 
-    @Query("SELECT p.* FROM CollectorFavoritePerformer cp JOIN Performer p on cp.performerId = p.id WHERE cp.collectorId = :collectorId")
-    fun getFavoritePerformersByCollectorId(collectorId: Int): Flow<List<Performer>>
+    @Query("SELECT p.* FROM CollectorFavoritePerformer cp JOIN Performer p on cp.performerId = p.id WHERE cp.collectorId = :collectorId ORDER BY p.name COLLATE UNICODE")
+    abstract fun getFavoritePerformersByCollectorId(collectorId: Int): Flow<List<Performer>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPerformers(musicians: List<Performer>)
+    protected abstract suspend fun insertPerformers(musicians: List<Performer>)
 
     @Query("DELETE FROM performer WHERE type = :performerType")
-    suspend fun deletePerformersByType(performerType: PerformerType)
+    protected abstract suspend fun deletePerformersByType(performerType: PerformerType)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAlbums(albums: List<Album>)
+    protected abstract suspend fun insertAlbums(albums: List<Album>)
 
-    // Internal use only
     @Insert
-    suspend fun insertPerformerAlbums(performerAlbum: List<PerformerAlbum>)
+    protected abstract suspend fun insertPerformerAlbums(performerAlbum: List<PerformerAlbum>)
 
-    // Internal use only
     @Transaction
     @Query("DELETE FROM PerformerAlbum WHERE performerId IN (SELECT id FROM Performer WHERE Performer.type = :performerType)")
-    suspend fun deletePerformerAlbumsByPerformerType(performerType: PerformerType)
+    protected abstract suspend fun deletePerformerAlbumsByPerformerType(performerType: PerformerType)
 
     @Query("DELETE FROM PerformerAlbum WHERE performerId = :performerId")
-    suspend fun deletePerformerAlbumsByPerformerId(performerId: Int)
+    protected abstract suspend fun deletePerformerAlbumsByPerformerId(performerId: Int)
 
-    // Internal use only
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCollectors(collectors: List<Collector>)
+    protected abstract suspend fun insertCollectors(collectors: List<Collector>)
 
-    // Internal use only
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCollectorFavoritePerformers(collectorFavoritePerformers: List<CollectorFavoritePerformer>)
+    protected abstract suspend fun insertCollectorFavoritePerformers(collectorFavoritePerformers: List<CollectorFavoritePerformer>)
 
     @Transaction
     @Query("DELETE FROM CollectorFavoritePerformer WHERE performerId IN (SELECT id FROM Performer WHERE Performer.type = :performerType)")
-    suspend fun deleteCollectorFavoritePerformerByPerformerType(performerType: PerformerType)
+    protected abstract suspend fun deleteCollectorFavoritePerformerByPerformerType(performerType: PerformerType)
 
     @Query("DELETE FROM CollectorFavoritePerformer WHERE performerId = :performerId")
-    suspend fun deleteCollectorFavoritePerformerByPerformerId(performerId: Int)
+    protected abstract suspend fun deleteCollectorFavoritePerformerByPerformerId(performerId: Int)
 
     @Query("DELETE FROM MusicianBand")
-    suspend fun deleteMusicianBands()
+    protected abstract suspend fun deleteMusicianBands()
 
     @Query("DELETE FROM MusicianBand WHERE bandId = :bandId")
-    suspend fun deleteMusicianBandsByBandId(bandId: Int)
+    protected abstract suspend fun deleteMusicianBandsByBandId(bandId: Int)
 
     @Insert
-    suspend fun insertMusicianBands(musicianBands: List<MusicianBand>)
+    protected abstract suspend fun insertMusicianBands(musicianBands: List<MusicianBand>)
 
-    // Refresh the list of musicians and their collectors
-    // To make sure that the database is consistent it is necessary to update the collectors
+    // Refresh the list of musicians
+    // To make sure that the database is consistent it is necessary to update the collectors and the albums
     // associated with the musicians as well
+    //
+    // It is necessary to set deleteAll to remove musicians from the local database that are no longer available upstream
     @Transaction
-    suspend fun deleteAndInsertMusicians(musicians: List<MusicianJson>, deleteAll: Boolean = true) {
+    open suspend fun deleteAndInsertMusicians(musicians: List<MusicianJson>, deleteAll: Boolean = true) {
         val collectors: MutableList<Collector> = mutableListOf()
         val collectorFavoritePerformers: MutableList<CollectorFavoritePerformer> = mutableListOf()
 
@@ -140,11 +138,13 @@ interface PerformerDAO {
         insertPerformerAlbums(performerAlbums)
     }
 
-    // Refresh the list of bands and their collectors
-    // To make sure that the database is consistent it is necessary to update the collectors
-    // associated with the bands as well
+    // Refresh the list of bands
+    // To make sure that the database is consistent it is necessary to update the collectors, the albums and the members
+    // associated with the band as well
+    //
+    // It is necessary to set deleteAll to remove bands from the local database that are no longer available upstream
     @Transaction
-    suspend fun deleteAndInsertBands(bands: List<BandJson>, deleteAll: Boolean = true) {
+    open suspend fun deleteAndInsertBands(bands: List<BandJson>, deleteAll: Boolean = true) {
         val collectors: MutableList<Collector> = mutableListOf()
         val collectorFavoritePerformers: MutableList<CollectorFavoritePerformer> = mutableListOf()
 
