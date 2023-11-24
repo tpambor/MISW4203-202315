4f85324d14d6997720c27769aa71c1b73e0b1daa
Camilo Ramírez Restrepo <c.ramirezr2@uniandes.edu.co>
2023-11-23 20:41:56 -0500

    Solución a problemas de accesibilidad en listado de coleccionistas (#143)

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/ui/views/CollectorListScreen.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/ui/views/CollectorListScreen.kt
@@ -23,6 +23,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -79,7 +81,8 @@ fun CollectorListScreen(snackbarHostState: SnackbarHostState) {
         onRefresh = { viewModel.onRefresh() }
     )
 
-    Box(Modifier.pullRefresh(pullRefreshState)) {
+    Box(Modifier.pullRefresh(pullRefreshState)
+        .semantics { this.contentDescription = "Lista de coleccionistas" }) {
         CollectorList(collectors, userId)
 
         PullRefreshIndicator(
@@ -103,7 +106,8 @@ private fun CollectorItem(collector: CollectorWithPerformers, testTag: String) {
     ElevatedCard(
         modifier = Modifier
             .padding(8.dp)
-            .testTag(testTag),
+            .testTag(testTag)
+            .semantics { this.contentDescription = "Coleccionista" },
         onClick = { }
     ) {
         Column(
@@ -125,7 +129,7 @@ private fun CollectorItem(collector: CollectorWithPerformers, testTag: String) {
                 style = MaterialTheme.typography.bodyMedium,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
-                text = if (collector.performers.isEmpty()) "-" else
+                text = if (collector.performers.isEmpty()) "Sin artistas favoritos" else
                     collector.performers.joinToString { it.name }
             )
         }
