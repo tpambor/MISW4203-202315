31f92276295d551c6ff4644ad645ae09634ec667
Laura Molina <ld.molina11@uniandes.edu.co>
2023-11-30 01:40:00 -0500

    Scroll to button

--- app/src/androidTest/java/co/edu/uniandes/misw4203/equipo11/vinilos/pageobjects/AlbumCreate.kt
+++ app/src/androidTest/java/co/edu/uniandes/misw4203/equipo11/vinilos/pageobjects/AlbumCreate.kt
@@ -1,13 +1,15 @@
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import co.edu.uniandes.misw4203.equipo11.vinilos.pageobjects.PageObject
 import co.edu.uniandes.misw4203.equipo11.vinilos.ui.MainActivity
 
-class AlbumCreate(composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) : PageObject(composeTestRule) {
+class AlbumCreate(private val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) : PageObject(composeTestRule) {
     fun fillBasicInput(text: String, textTag: String) {
         findExactlyOne(
             hasTestTag(textTag)
@@ -25,6 +27,7 @@ class AlbumCreate(composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<M
 
 
     fun getSubmitButton() : SemanticsNodeInteraction {
+        composeTestRule.onNodeWithTag("album-submit").performScrollTo()
         return findExactlyOne(
             hasTestTag("album-submit")
         )
