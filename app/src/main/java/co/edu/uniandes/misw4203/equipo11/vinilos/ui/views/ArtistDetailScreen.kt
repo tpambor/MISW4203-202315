108964ada5521dec657613e91510aa4f0c63be71
Tim Pambor <t.pambor@uniandes.edu.co>
2023-12-01 18:38:55 +0100

    Cleanup

--- app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/ui/views/ArtistDetailScreen.kt
+++ app/src/main/java/co/edu/uniandes/misw4203/equipo11/vinilos/ui/views/ArtistDetailScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridScope
-import androidx.compose.foundation.lazy.grid.LazyGridScopeMarker
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
